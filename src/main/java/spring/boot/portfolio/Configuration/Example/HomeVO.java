fd35af815342e8f4415425a61deef73f8d650a3d
Leia1ra <leiaira92@gmail.com>
2024-01-01 23:13:26 +0900

    example 예제 작성

---
+++ src/main/java/spring/boot/portfolio/Configuration/Example/HomeVO.java
@@ -0,0 +1,11 @@
+package spring.boot.portfolio.Configuration.Example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class HomeVO {
+    private String name;
+    private int age;
+}
