11b818d0a484b28a73344ff49991bdbba58c2128
dolto <hdw810@daum.net>
2024-01-01 15:44:40 +0900

    test

--- src/main/java/spring/boot/portfolio/PortfolioApplication.java
+++ src/main/java/spring/boot/portfolio/PortfolioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PortfolioApplication {
 
     public static void main(String[] args) {
+        //여기서 개발자는 공동으로 이선왕, 곽병준, 홍도완이 진행합니다.
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
