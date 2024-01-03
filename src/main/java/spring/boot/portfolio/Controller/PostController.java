d549a7f1fe2b83714dbb75a22e51ee7044737c2a
dolto <hdw810@daum.net>
2024-01-03 16:05:10 +0900

    마크다운과 카테고리 생성 및 예외처리

--- src/main/java/spring/boot/portfolio/Controller/PostController.java
+++ src/main/java/spring/boot/portfolio/Controller/PostController.java
@@ -2,8 +2,10 @@ package spring.boot.portfolio.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import spring.boot.portfolio.Model.CategoryModel.CategoryCollection;
 import spring.boot.portfolio.Model.CategoryModel.PostModel.ContentMode;
 import spring.boot.portfolio.Model.CategoryModel.PostModel.PostCollection;
 import spring.boot.portfolio.Model.CategoryModel.PostModel.PostContent;
@@ -27,7 +29,11 @@ public class PostController {
         return postService.findAll();
     }
     @RequestMapping("/PostInsertPage")
-    public String PostInsertPage(){
+    public String PostInsertPage(Model model){
+        List<String> categoryList = postService.findCategoryAll().stream().map(
+                CategoryCollection::getName).toList();
+        model.addAttribute("categoryNames", categoryList);
+        //카테고리 이름 모음을 만들어서, 게시글 작성 시 존재하는 카테고리에 데이터를 추가할지 새로 만들지 선택 가능하게
         return "Post/PostInsert";
     }
     @RequestMapping("/PostInsertAction")
@@ -38,6 +44,12 @@ public class PostController {
         PostCollection temp = new PostCollection(post_name, contents, "test");
 
         postService.CategoryInputPostId(postService.postSave(temp).getId(),post_category);
+        return "redirect:/PostInsertPage";
+    }
+
+    @RequestMapping("/AddCategory")
+    public String AddCategory(String category_name){
+        postService.addCategory(category_name);
         return "redirect:/PostInsertPage";
     }
 }
